24d9c5f2374fffe05538e3fda4ec86da786488f9
Abdullah <mafishere@gmail.com>
2024-09-29 14:40:54 +0500

    changed navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css";
 
 const Navbar = () => {
   const [loggedIn, setLoggedIn] = useState(true);
