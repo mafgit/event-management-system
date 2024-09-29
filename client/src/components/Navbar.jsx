3ed5d0e798efe2226eb02cb04c07ca31303e1de7
Abdullah <mafishere@gmail.com>
2024-09-29 14:40:05 +0500

    changed navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+
   return (
     <div className="bg-slate-900 text-white py-3 px-4 flex justify-between">
       <Link to="/">
         <h1>EMS</h1>
       </Link>
-      <div className="flex gap-4">
+      <div className="navbar-links flex gap-4 relative">
         <Link to="#">Home</Link>
         <Link to="#">Find Events</Link>
-        <Link to="#">Account</Link>
+        <Link to="#">Create Event</Link>
+        {loggedIn ? (
+          <>
+            <Link to="#">Organized by me</Link>
+            <Link to="#">Visited</Link>
+            <Link to="#">Account</Link>
+          </>
+        ) : (
+          <Link to="#">Login</Link>
+        )}
       </div>
     </div>
   );
