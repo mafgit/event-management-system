a2d85a3f837f46bac9308efba145173918003f34
Abdullah <mafishere@gmail.com>
2024-09-30 22:20:34 +0500

    added event page

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,5 +1,87 @@
+import { FaUser } from "react-icons/fa6";
+import Review from "../components/Review";
+
 const Event = () => {
-  return <div></div>;
+  return (
+    <div className="mx-[50px] my-[25px]">
+      <div className="w-full h-[350px] relative">
+        <img
+          src="/form-bg-1.jpg"
+          className="w-full h-full object-cover rounded-md"
+          alt="event image"
+        />
+        <div className="absolute left-0 top-0 w-full h-full opacity-50 bg-black z-20"></div>
+        <h1 className="absolute left-6 bottom-5 z-30 text-white text-3xl font-bold">
+          Event Name
+        </h1>
+      </div>
+
+      <div className="flex my-[10px] gap-2 items-stretch">
+        <div className="bg-slate-300 p-5 rounded-md grow-[4] basis-0 flex flex-col gap-3">
+          {/* details etc */}
+          <div className="flex flex-wrap gap-3">
+            <div className="flex gap-2 px-2 py-0 items-center justify-center w-max bg-white rounded-full">
+              <FaUser /> 250
+            </div>
+
+            <div className="flex gap-2 px-2 py-0 items-center justify-center w-max bg-white rounded-full">
+              <FaUser /> Lorem ipsum dolor sit.
+            </div>
+
+            <div className="flex gap-2 px-2 py-0 items-center justify-center w-max bg-white rounded-full">
+              <FaUser /> Lorem, ipsum.
+            </div>
+
+            <div className="flex gap-2 px-2 py-0 items-center justify-center w-max bg-white rounded-full">
+              <FaUser /> 250
+            </div>
+
+            <div className="flex gap-2 px-2 py-0 items-center justify-center w-max bg-white rounded-full">
+              <FaUser /> 250
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            {/* desc etc */}
+            <p>
+              <b>Organized by: </b> Sarim Asif
+            </p>
+            <p className="italic">
+              <b>Description:</b> Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Nisi, porro sit? Aperiam, dolores corrupti
+              corporis voluptate alias sunt officiis aut?
+            </p>
+          </div>
+        </div>
+        <div className="grow-[1] w-full h-full bg-slate-300 rounded-md basis-0 p-5">
+          {/* tickets etc */}
+          <div className="flex flex-col gap-2">
+            <button className="text-white bg-gradient-to-r from-pink-700 to-blue-700 p-2 w-full rounded-md font-bold uppercase">
+              Register (Normal)
+            </button>
+
+            <div className="flex">
+              <h1 className="flex flex-col text-center basis-0 grow-[1] w-full items-center justify-center">
+                <b>249</b> Tickets left
+              </h1>
+              <div className="flex basis-0 grow-[1] w-full items-center justify-center bg-white rounded-full">
+                <h1 className="text-gradient text-xl font-bold">PKR 999</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* reviews */}
+      <div className="">
+        <h1 className="text-xl">Reviews</h1>
+        <div className="">
+          {[1, 2, 3, 4, 5].map((i) => (
+            <Review />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Event;
