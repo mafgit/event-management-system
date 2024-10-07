7dd05ffeb888a1b4cf1e288c8cc787c8395a3815
Sarim Bin Asif <sarimbinasif12345@gmail.com>
2024-10-07 16:45:24 +0500

    added Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import { FaGithub, FaEnvelope, FaInfoCircle } from "react-icons/fa"; 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-300 text-black py-4 px-6 flex justify-between items-center">
+      <div className="footer-links flex gap-5">
+        <Link to="https://github.com/mafgit/event-management-system" target="_blank" className="flex items-center gap-2">
+          <FaGithub /> Github
+        </Link>
+        <Link to="/contact" className="flex items-center gap-2">
+          <FaEnvelope /> Contact Us
+        </Link>
+        <Link to="/about" className="flex items-center gap-2">
+          <FaInfoCircle /> About Us
+        </Link>
+      </div>
+      <div className="text-sm italic">&copy; 2024 Event Management System</div>
+    </div>
+  );
 };
 
 export default Footer;
